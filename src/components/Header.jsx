20328598b86f723d2a329c711a20339584013325
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-11-03 19:45:00 -0300

    Requisito 4 - completo - Nav/Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -25,13 +26,19 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         Header
-        <div data-testid="header-user-name">
-          {
-            isLoading === true
-              ? <Loading />
-              : userName
-          }
-        </div>
+        {
+          isLoading === true
+            ? <Loading />
+            : (
+              <div data-testid="header-user-name">
+                { userName }
+                <nav>
+                  <Link data-testid="link-to-search" to="/search">Search</Link>
+                  <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+                  <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+                </nav>
+              </div>)
+        }
       </header>
     );
   }
