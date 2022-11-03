7f03fe8657e0e6216f57ba6fb1d9c6013fc8dafc
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-11-03 17:52:54 -0300

    Requisito 2 - completo Loading/Redirect

--- src/App.js
+++ src/App.js
@@ -7,19 +7,84 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  state = {
+    inputNameValue: '',
+    isDisable: true,
+    isLoading: false,
+    isClicked: false,
+  };
+
+  btnLoginValidation = () => {
+    const { inputNameValue } = this.state;
+    const maxCaract = 3;
+
+    if (inputNameValue.length >= maxCaract) {
+      this.setState({ isDisable: false });
+    }
+  };
+
+  requestApi = async (event) => {
+    event.preventDefault();
+
+    this.setState({
+      isLoading: true,
+      isClicked: true,
+    });
+
+    const { inputNameValue } = this.state;
+    const userName = await createUser({ name: inputNameValue });
+
+    if (userName) {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  onChangeHandler = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox'
+      ? target.checked
+      : target.value;
+
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.btnLoginValidation();
+    });
+  };
+
   render() {
+    const {
+      inputNameValue,
+      isDisable,
+      isLoading,
+      isClicked,
+    } = this.state;
+
     return (
       <BrowserRouter>
         <p>TrybeTunes</p>
         <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/search" component={ Search } />
-          <Route exact path="/album/:id" component={ Album } />
-          <Route exact path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              <Login
+                inputNameValue={ inputNameValue }
+                isDisable={ isDisable }
+                onChangeHandler={ this.onChangeHandler }
+                requestApi={ this.requestApi }
+                isLoading={ isLoading }
+                isClicked={ isClicked }
+              />) }
+          />
           <Route exact path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
 
 export default class Login extends Component {
   render() {
+    const {
+      inputNameValue,
+      isDisable,
+      onChangeHandler,
+      isLoading,
+      requestApi,
+      isClicked,
+    } = this.props;
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        Login
+        <form>
+          <fieldset>
+            <div>
+              <input
+                type="text"
+                id="inputName"
+                name="inputNameValue"
+                value={ inputNameValue }
+                placeholder="Digite seu nome de Login"
+                onChange={ onChangeHandler }
+                data-testid="login-name-input"
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={ isDisable }
+              onClick={ requestApi }
+              data-testid="login-submit-button"
+            >
+              Entrar
+            </button>
+          </fieldset>
+        </form>
+        {isLoading && (<Loading />)}
+        {(isLoading === false && isClicked) && (<Redirect to="/search" />)}
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  inputNameValue: PropTypes.string.isRequired,
+  isDisable: PropTypes.bool.isRequired,
+  onChangeHandler: PropTypes.func.isRequired,
+  requestApi: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  isClicked: PropTypes.bool.isRequired,
+};
